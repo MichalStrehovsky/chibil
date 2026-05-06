bd3b3961d714ba900e9c25a59cf6db468578c643
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2026-05-05 20:54:53 -0700

    Add vcvars.cmd for Visual Studio environment setup

---
+++ .github/workflows/vcvars.cmd
@@ -0,0 +1,28 @@
+SET vswherePath=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+IF NOT EXIST "%vswherePath%" GOTO :ERROR
+
+SET toolsSuffix=x86.x64
+IF /I "%~1"=="arm64" SET toolsSuffix=ARM64
+
+FOR /F "tokens=*" %%i IN (
+    '"%vswherePath%" -latest -prerelease -products * ^
+    -requires Microsoft.VisualStudio.Component.VC.Tools.%toolsSuffix% ^
+    -property installationPath'
+) DO SET vsBase=%%i
+
+IF "%vsBase%"=="" GOTO :ERROR
+
+IF /I "%PROCESSOR_ARCHITECTURE%" == "ARM64" (
+    IF /I "%~1" == "x64"   ( set vcEnvironment=arm64_amd64 )
+    IF /I "%~1" == "x86"   ( set vcEnvironment=arm64_x86 )
+    IF /I "%~1" == "arm64" ( set vcEnvironment=arm64 )
+) ELSE (
+    IF /I "%~1" == "x64"   ( set vcEnvironment=amd64 )
+    IF /I "%~1" == "x86"   ( set vcEnvironment=amd64_x86 )
+    IF /I "%~1" == "arm64" ( set vcEnvironment=amd64_arm64 )
+)
+
+CALL "%vsBase%\vc\Auxiliary\Build\vcvarsall.bat" %vcEnvironment% > NUL 2> NUL
+
+:ERROR
+EXIT /B 1
