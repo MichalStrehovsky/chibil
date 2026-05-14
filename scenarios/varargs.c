c3ebd9b18d0e3407dc5bd30556d7f54d81800d84
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2026-05-14 13:11:55 +0900

    Delete alloca and varargs

--- scenarios/varargs.c
+++
@@ -1,9 +0,0 @@
-// COMPILE: cl /c /Z7 /Zl /d1clrNoPureCRT /clr /BC varargs.c
-// LINK: link varargs.obj mscoree.lib /incremental:no /debug /entry:main /subsystem:console
-
-int __cdecl sum(int count, ...);
-
-int main()
-{
-    return sum(3, 10, 20, 30);
-}
